5ff3cdc3d95fc9ba2f1cd640b206c549423d7cde
sharmaharshsuresh <sharmaharshsuresh@outlook.com>
2023-10-26 17:58:03 +0530

    OpenClose Principle Project Added

---
+++ Open_Close_Principle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Close_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
