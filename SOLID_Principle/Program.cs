4585963b51b783a09d4c42f47debe3a8061c0794
sharmaharshsuresh <sharmaharshsuresh@outlook.com>
2023-10-26 17:10:38 +0530

    Add project files.

---
+++ SOLID_Principle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace SOLID_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UserService userService = new UserService(new EmailService());
+            userService.Register("harsh@gmail.com", "123456");
+            Console.ReadLine();
+        }
+        public class UserService
+        {
+            EmailService emailService;
+            public UserService(EmailService aEmailService)
+            {
+                emailService = aEmailService;
+            }
+            public void Register(string email, string password)
+            {
+                if (!emailService.ValidateEmail(email))
+                {
+                    Console.WriteLine("Invalid User.");
+                }
+                else
+                {
+                    Console.WriteLine("User Added/Register.");
+                    emailService.SendEmail(new MailMessage("myname@mydomain.com", email) { Subject = "Hi. How are you!" });
+                }
+            }
+        }
+        public class EmailService
+        {
+            public EmailService()
+            {
+                
+            }
+            public bool ValidateEmail(string email)
+            {
+                Console.WriteLine("Doing simple email validation.");
+                Console.WriteLine("Validating @ present in email or not.");
+                return email.Contains("@");
+            }
+            public void SendEmail(MailMessage message)
+            {
+                //_smtpClient.Send(message);
+                Console.WriteLine("Email Sent to new user.");
+            }
+        }
+    }
+}
