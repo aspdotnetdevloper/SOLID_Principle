dccfadbbf43dfd3bf4dfd1e53cb31c0e97e853af
sharmaharshsuresh <sharmaharshsuresh@outlook.com>
2023-10-27 15:31:09 +0530

    New Project added

---
+++ Liskov_Substitution_Principle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Liskov_Substitution_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
